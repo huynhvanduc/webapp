129f051382f7288e38d8e66c09349470f7df3cb6
huynhvanduc <vanduc9x98@gmail.com>
2020-09-24 14:50:11 +0700

    + Chapter 21 (xong)

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using WebApp.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.OpenApi.Models;
 
 namespace WebApp
 {
@@ -22,9 +13,6 @@ namespace WebApp
             Configuration = config;
         }
         public IConfiguration Configuration { get; set; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(opts => {
@@ -32,49 +20,17 @@ namespace WebApp
                 "ConnectionStrings:ProductConnection"]);
                 opts.EnableSensitiveDataLogging(true);
             });
-
-            services.AddControllers().AddNewtonsoftJson().AddXmlSerializerFormatters();
-
-            services.Configure<MvcNewtonsoftJsonOptions>(opts => {
-                opts.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
-            });
-
-            services.Configure<MvcOptions>(opts => {
-                opts.RespectBrowserAcceptHeader = true;
-                opts.ReturnHttpNotAcceptable = true;
-            });
-
-            services.AddSwaggerGen(options => {
-                options.SwaggerDoc("v1",
-                new OpenApiInfo { Title = "WebApp", Version = "v1" });
-            });
-
-            //services.Configure<JsonOptions>(opts => {
-            //    opts.JsonSerializerOptions.IgnoreNullValues = true;
-            //});
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
-
-
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, DataContext context)
         {
             app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
             app.UseRouting();
-            app.UseMiddleware<TestMiddleware>();
             app.UseEndpoints(endpoints => {
-                endpoints.MapGet("/", async context => {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-                // endpoints.MapWebService();
                 endpoints.MapControllers();
+                endpoints.MapDefaultControllerRoute();
             });
-
-            app.UseSwagger();
-            app.UseSwaggerUI(options => {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApp");
-            });
-
             SeedData.SeedDatabase(context);
         }
     }
